361376f1709128909ed9d84495a33fe86bba28c3
Minified <gora.pub@gmail.com>
2016-11-19 02:42:45 +0100

    explicite ok

--- test/test_server_suite.erl
+++ test/test_server_suite.erl
@@ -7,14 +7,14 @@
 
 get_listensocket__1_test() ->
   {ok,LSock} = test_server:get_lsock(),
-  gen_tcp:close(LSock).
+  ok = gen_tcp:close(LSock).
 get_listensocket__2_test() ->
   {ok,LSock} = test_server:get_lsock(),
   {error,_} = test_server:get_lsock(),
-  gen_tcp:close(LSock).
+  ok = gen_tcp:close(LSock).
 
 get_socket__1_test() ->
   spawn(fun() -> timer:sleep(3),test_server:connect_worker() end),
   {ok, Socket, ListenSock} = test_server:get_socket(),
-  gen_tcp:close(Socket),
-  gen_tcp:close(ListenSock).
+  ok = gen_tcp:close(Socket),
+  ok = gen_tcp:close(ListenSock).
