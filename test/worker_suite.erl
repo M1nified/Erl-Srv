22a5e8ecaf5141fc96a7ecbb84b0dbe9e71bdd6f
Minified <gora.pub@gmail.com>
2016-11-19 02:54:13 +0100

    test conn timeout raised

--- test/worker_suite.erl
+++ test/worker_suite.erl
@@ -9,14 +9,14 @@ jms_1() ->
   }.
 
 worker__1_test() ->
-  spawn(fun() -> timer:sleep(3),test_server:connect_worker() end),
+  spawn(fun() -> timer:sleep(10),test_server:connect_worker() end),
   {ok, Socket, ListenSock} = test_server:get_socket(),
   JMS = jms_1(),
   WorkerThread = worker:spawn(Socket,JMS),
   #thread{pid = WorkerPid, ref = WorkerRef} = WorkerThread,
   WorkerPid = WorkerThread#thread.pid,
   WorkerRef = WorkerThread#thread.ref,
-  
+
   gen_tcp:close(Socket),
   gen_tcp:close(ListenSock).
 
