3c74a1efc055bd351144dbba1b21942105890cb2
Michał Góra <Micha Gra>
2017-01-24 10:49:22 +0100

    if ports declared

--- headers/_settings.hrl
+++ headers/_settings.hrl
@@ -2,13 +2,18 @@
 -define(DEBUG, true).
 -endif.
 
+-ifndef(WORKER_PORT).
 -define(WORKER_PORT, 5678).
+-endif.
+-ifndef(DISPLAY_PORT).
 -define(DISPLAY_PORT, 8765).
+-endif.
 
 -define(JOB,example_job).
 -define(WORKER,example_worker).
 
 -define(SERVER_ADDR, "localhost").
+% -define(SERVER_ADDR, "149.156.207.22"). 
 
 -define(WORKER_LISTEN_OPTIONS, [
                                   binary,
